a1dd7285abbfabe116f667a9d270ba9a3fd5dc0e
alejandrols12 <alopezs5@alumnos.nebrija.es>
2024-04-07 10:31:16 +0200

    login slyte

--- islands/Login.tsx
+++ islands/Login.tsx
@@ -35,19 +35,21 @@ const Login: FunctionComponent<{loged?: string}> = ({loged}) => {
   //to get token on client console.log("-------document----",IS_BROWSER && document.cookie);
   if(!loged){
     return (
-      <form action="/" method="post">
-          <select name="usuario"  value ="Admin" onChange={(e)=>{setuser(e.currentTarget.value)}}>
+      <div class="login">
+        <form action="/" method="post">
+          <select name="usuario"  value ={user} onChange={(e)=>{setuser(e.currentTarget.value)}}>
               <option value="Admin">Admin</option>
               <option value="Espe">Esperanza</option>
               <option value="Angel">Angel</option>
               <option value="Jose">Jose</option>   
           </select>    
           <input name="password" placeholder="Contraseña" onBlur={(e)=>{setpass(e.currentTarget.value); seterror(""); }}></input>
-          <button type="submit" disabled={false} onClick={async (e)=>{check(e)}}>Log-in</button>
+          <button type="submit" disabled={false} onClick={ (e)=>{check(e)}}>Log-in</button>
           {error && error}
           
       
-      </form>
+        </form>
+      </div>
     );
   }else{
 
